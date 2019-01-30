cafc7c6c98bb39e30f269a4fd4e4b6c64d75e6c6
Ophirus Magnivus <dreckguy@gmail.com>
2019-01-30 06:15:18 +0200

    serving the encrypted text!

--- server.py
+++ server.py
@@ -5,13 +5,9 @@ from flask import Flask
 import urllib
 ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
 encryptedFile = urllib.request.urlopen(ENCRYPTED_FILE_ADDRESS)
-for line in encryptedFile:
-    print(line)
-
-
-
+encryptedText = encryptedFile.readline()
 
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return ENCRYPTED_FILE_ADDRESS
+    return encryptedText
