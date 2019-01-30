1858997613fac4c90a92fc55355f17307d4a6325
Ophirus Magnivus <dreckguy@gmail.com>
2019-01-30 18:13:56 +0200

    jsonify the response

--- server.py
+++ server.py
@@ -2,22 +2,27 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask
+from flask import jsonify
+
 import urllib
 ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
 encryptedFile = urllib.request.urlopen(ENCRYPTED_FILE_ADDRESS)
-encryptedText = encryptedFile.readline()
+encryptedMessage = encryptedFile.readline()
+
+messageResponse = {'temperature':'24.3'}
+
+statusResponse = "status"
 
 SHOW_MESSAGE_ROUTE = os.getenv("SHOW_MESSAGE_ROUTE")
 SHOW_STATUS_ROUTE = os.getenv("SHOW_STATUS_ROUTE")
 
-
 app = Flask(__name__)
 @app.route(SHOW_MESSAGE_ROUTE)
 def showMessage():
-    return encryptedText
+    return jsonify(messageResponse)
 @app.route(SHOW_STATUS_ROUTE)
 def showStatus():
-    return "status"
+    return statusResponse
 
 
 
