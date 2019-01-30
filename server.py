9444c214876ede7bfac417d04a3a65b0f2c6e069
Ophirus Magnivus <dreckguy@gmail.com>
2019-01-30 21:41:11 +0200

    add helper route in root to show connection

--- server.py
+++ server.py
@@ -3,26 +3,39 @@ from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask
 from flask import jsonify
-
 import urllib
 ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
 encryptedFile = urllib.request.urlopen(ENCRYPTED_FILE_ADDRESS)
 encryptedMessage = encryptedFile.readline()
 
-messageResponse = {'temperature':'24.3'}
 
-statusResponse = "status"
+LINK_TO_GITHUB = os.getenv("LINK_TO_GITHUB")
+LINK_TO_DOKERHUB = os.getenv("LINK_TO_DOKERHUB")
+
+
+statusResponse = {}
+statusResponse['status'] = "completed"
+statusResponse['container'] = LINK_TO_DOKERHUB
+statusResponse['project'] = LINK_TO_GITHUB
+
+messageResponse = {}
+messageResponse['message'] = "encryptedMessage"
 
 SHOW_MESSAGE_ROUTE = os.getenv("SHOW_MESSAGE_ROUTE")
 SHOW_STATUS_ROUTE = os.getenv("SHOW_STATUS_ROUTE")
 
+
 app = Flask(__name__)
+@app.route('/')
+def showConnection():
+    return "Server is up and running!"
 @app.route(SHOW_MESSAGE_ROUTE)
 def showMessage():
     return jsonify(messageResponse)
+
 @app.route(SHOW_STATUS_ROUTE)
 def showStatus():
-    return statusResponse
+    return jsonify(statusResponse)
 
 
 
