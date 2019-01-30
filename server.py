6063331b7d8498876dd09e7c180187f7eddd4ea6
Ophirus Magnivus <dreckguy@gmail.com>
2019-01-30 17:45:40 +0200

    split to the two needed routes

--- server.py
+++ server.py
@@ -7,7 +7,18 @@ ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
 encryptedFile = urllib.request.urlopen(ENCRYPTED_FILE_ADDRESS)
 encryptedText = encryptedFile.readline()
 
+SHOW_MESSAGE_ROUTE = os.getenv("SHOW_MESSAGE_ROUTE")
+SHOW_STATUS_ROUTE = os.getenv("SHOW_STATUS_ROUTE")
+
+
 app = Flask(__name__)
-@app.route("/")
-def hello():
+@app.route(SHOW_MESSAGE_ROUTE)
+def showMessage():
     return encryptedText
+@app.route(SHOW_STATUS_ROUTE)
+def showStatus():
+    return "status"
+
+
+
+
