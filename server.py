b5b8de743e1538f592be7212a2a9251212906e94
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-02 00:08:26 +0200

    add decode unfinished function

--- server.py
+++ server.py
@@ -6,26 +6,33 @@ from flask import Flask
 from flask import jsonify
 import urllib2
 
+
 SHOW_MESSAGE_ROUTE = '/message'
 SHOW_STATUS_ROUTE = '/status'
 LINK_TO_GITHUB='https://github.com/dreckguy/decryption-challenge'
 LINK_TO_DOKERHUB='https://hub.docker.com/r/dreckguy/decryption-challenge'
 
 ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
+
 SECRET_KEY=os.getenv('SECRET_KEY')
+encryptedFile = urllib2.urlopen(ENCRYPTED_FILE_ADDRESS)
+encryptedMessage = encryptedFile.readline().decode("utf-8").strip('\n')
+decrypedMessage = decode(encryptedMessage,SECRET_KEY)
+messageResponse = {}
+messageResponse['message'] = decrypedMessage
+
+def decode(encrypted,key): # need to write the function
+    return encrypted 
 
 app = Flask(__name__)
 
 @app.route('/')
 def showConnection():
     return "Server is up and running!\n"
-        
+
 @app.route(SHOW_MESSAGE_ROUTE)
 def showMessage():
-    encryptedFile = urllib2.urlopen(ENCRYPTED_FILE_ADDRESS)
-    encryptedMessage = encryptedFile.readline().decode("utf-8").strip('\n')
-    messageResponse = {}
-    messageResponse['message'] = encryptedMessage
+    
     return jsonify(messageResponse)
 
 @app.route(SHOW_STATUS_ROUTE)
