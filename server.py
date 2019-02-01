1081510e107eaffec83973c79bb3ca9742dbc2d3
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-01 13:07:22 +0200

    change functions structure to use urllib inside function call

--- server.py
+++ server.py
@@ -4,23 +4,8 @@ load_dotenv()
 from flask import Flask
 from flask import jsonify
 import urllib
-ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
-encryptedFile = urllib.request.urlopen(ENCRYPTED_FILE_ADDRESS)
-encryptedMessage = encryptedFile.readline().decode("utf-8").strip('\n')
-print(encryptedMessage)
 
 
-LINK_TO_GITHUB = os.getenv("LINK_TO_GITHUB")
-LINK_TO_DOKERHUB = os.getenv("LINK_TO_DOKERHUB")
-
-
-statusResponse = {}
-statusResponse['status'] = "completed"
-statusResponse['container'] = LINK_TO_DOKERHUB
-statusResponse['project'] = LINK_TO_GITHUB
-
-messageResponse = {}
-messageResponse['message'] = encryptedMessage
 
 SHOW_MESSAGE_ROUTE = os.getenv("SHOW_MESSAGE_ROUTE")
 SHOW_STATUS_ROUTE = os.getenv("SHOW_STATUS_ROUTE")
@@ -32,10 +17,22 @@ def showConnection():
     return "Server is up and running!\n"
 @app.route(SHOW_MESSAGE_ROUTE)
 def showMessage():
+    ENCRYPTED_FILE_ADDRESS = os.getenv("ENCRYPTED_FILE_ADDRESS")
+    encryptedFile = urllib.request.urlopen(ENCRYPTED_FILE_ADDRESS)
+    encryptedMessage = encryptedFile.readline().decode("utf-8").strip('\n')
+    messageResponse = {}
+    messageResponse['message'] = encryptedMessage
     return jsonify(messageResponse)
 
 @app.route(SHOW_STATUS_ROUTE)
 def showStatus():
+    LINK_TO_GITHUB = os.getenv("LINK_TO_GITHUB")
+    LINK_TO_DOKERHUB = os.getenv("LINK_TO_DOKERHUB")
+    
+    statusResponse = {}
+    statusResponse['status'] = "completed"
+    statusResponse['container'] = LINK_TO_DOKERHUB
+    statusResponse['project'] = LINK_TO_GITHUB
     return jsonify(statusResponse)
 
 
